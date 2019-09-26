f9c04c7f64b4778d6cf04c20da616255607fc7c9
BETeng <brian7.teng@gmail.com>
2019-09-25 22:55:29 -0500

    added morgan

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-const postRoutes = require('./routes/post');
+// routes
+const { getPosts } = require('./routes/post');
 
-app.get("/", postRoutes.getPosts);
+// middleware
+app.use(morgan("dev"))
 
+
+
+
+app.get("/", getPosts);
 
 const port = 8080;
 app.listen(port, () => {console.log(`API is listening on port: ${port}`)});
