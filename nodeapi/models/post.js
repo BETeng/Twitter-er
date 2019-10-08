cd1d207e617b50d2da6d0347253c84ffdc0a1426
BETeng <brian7.teng@gmail.com>
2019-10-07 22:56:56 -0500

    readded packages and chnaged req on post

--- nodeapi/models/post.js
+++ nodeapi/models/post.js
@@ -4,13 +4,13 @@ const postSchema = new mongoose.Schema({
   title:{
     type: String,
     required: "Title is required",
-    minlength: 4,
+    minlength: 2,
     maxlength: 150
   },
   body:{
     type: String,
     required: "Body is required",
-    minlength: 4,
+    minlength: 1,
     maxlength: 2000
   }
 });
